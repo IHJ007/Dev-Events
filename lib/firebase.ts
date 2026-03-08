e53507943f55ab097a75394237aea84517960072
Irfan H. Jesun <irfanhossainjesun@gmail.com>
2026-03-09 03:36:16 +0600

    Fix deployment error

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAN17zY6lU-pphxjGLnbC0lb0EWCUaWYnk",
-  authDomain: "dev-events-33e93.firebaseapp.com",
-  projectId: "dev-events-33e93",
-  storageBucket: "dev-events-33e93.firebasestorage.app",
-  messagingSenderId: "739510535072",
-  appId: "1:739510535072:web:6ddeaff2fcbfbe7158dac5",
-  measurementId: "G-EVWR1WG69G"
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Prevent re-initializing on hot reload
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
+export const db = getFirestore(app);
