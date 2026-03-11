3bf5c22f2060f50ade541e7cd0f3f89cb2fd2096
Irfan H. Jesun <irfanhossainjesun@gmail.com>
2026-03-12 01:59:32 +0600

    Added slug based routing

---
+++ app/api/events/[slug]/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/firebaseAdmin";
+import { NextRequest, NextResponse } from "next/server";
+import { EventItem } from "@/lib/constants";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { slug: string } }
+) {
+    try {
+        const snapshot = await db.collection("events")
+        .where("slug", "==", params.slug).get();
+
+        if (snapshot.empty) {
+            return NextResponse.json(
+                { error: "Event not found" },
+                { status: 404 }
+            );
+        }
+
+        const doc = snapshot.docs[0];
+        const data = doc.data() as EventItem 
+
+        return NextResponse.json({ id: doc.id, ...data })
+
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to fetch event" },
+            { status: 500 }
+        );
+    }
+}
