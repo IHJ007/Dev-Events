4698fa6668f39f60a6e6f333f72b1ee5a64991c7
Irfan H. Jesun <irfanhossainjesun@gmail.com>
2026-03-13 04:55:17 +0600

    Created EventDetails page and added basic things

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -24,7 +24,7 @@ function EventCard({ title, image, slug, location, date, time } : Props) {
   };
   console.log("image value:", image);
   return (
-    <Link href = {`/event/${slug}`} id='event-card' onClick={handleClick}>
+    <Link href = {`/events/${slug}`} id='event-card' onClick={handleClick}>
         <Image src = {image} alt = {title} width = {410} height = {300} className = 'poster'/>
 
         <div className='flex flex-row gap-2'>
